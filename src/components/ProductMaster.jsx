4e25185bae8366ed579bff1b6bf877342f0500c7
Prashant <desaniprashant@gmail.com>
2025-01-29 13:27:18 +0530

    Update ProductMaster.jsx

--- src/components/ProductMaster.jsx
+++ src/components/ProductMaster.jsx
@@ -34,11 +34,12 @@ function ProductMaster() {
   const fetchVendor = async () => {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/product`
+        `${process.env.REACT_APP_API_URL}/productAll`
       ); // Replace with your API URL
       //const data = await response.json();
+     
       setProducts(response.data.data);
-      console.log(response.data.data, "data");
+      console.log("data", response.data.data);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
